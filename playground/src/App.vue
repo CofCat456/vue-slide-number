f7e0b8bcef4fcf5ad091482a45ec34b43e9d7edb
CofCat456 <a25690756@gmail.com>
2024-04-28 13:47:27 +0800

    feat: poc working

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -11,28 +11,34 @@ function changeNumber(val: number) {
 </script>
 
 <template>
-  <main font-mono w-screen h-dvh grid="~ place-items-center" text="2xl center gray-700 dark:gray-200">
-    <div min-w-2xl aspect-ratio-square flex="~ items-center gap-2">
+  <main w-screen font-mono h-dvh grid="~ place-items-center" text="2xl center gray-700 dark:gray-200">
+    <div aspect-ratio-square min-w-2xl flex="~ items-center gap-2">
       <AnimationsNumber flex-1 :number />
 
       <aside text-base flex="~ col gap-5">
         <button type="button" class="btn" @click="changeNumber(1)">
           +1
         </button>
-        <button type="button" class="btn" @click="changeNumber(10)">
-          +10
+        <button type="button" class="btn" @click="changeNumber(15)">
+          +15
         </button>
-        <button type="button" class="btn" @click="changeNumber(100)">
-          +100
+        <button type="button" class="btn" @click="changeNumber(105)">
+          +105
+        </button>
+        <button type="button" class="btn" @click="changeNumber(1005)">
+          +1005
         </button>
         <button type="button" class="btn" @click="changeNumber(-1)">
           -1
         </button>
-        <button type="button" class="btn" @click="changeNumber(-10)">
-          -10
+        <button type="button" class="btn" @click="changeNumber(-15)">
+          -15
         </button>
-        <button type="button" class="btn" @click="changeNumber(-100)">
-          -100
+        <button type="button" class="btn" @click="changeNumber(-105)">
+          -105
+        </button>
+        <button type="button" class="btn" @click="changeNumber(-1005)">
+          -1005
         </button>
       </aside>
     </div>
--- src/components/AnimationsNumber.ts
+++ src/components/AnimationsNumber.ts
@@ -1,21 +1,46 @@
-import { Transition, defineComponent, h } from 'vue'
+import { Transition, defineComponent, h, ref, watch } from 'vue'
 
 export const AnimationsNumber = defineComponent(
   (props) => {
+    const numberList = ref<string[]>([])
+
+    const isLenChange = ref(false)
+    const isIncrease = ref(false)
+    const isNegative = ref(false)
+
+    watch(() => props.number, (newVal, oldVal) => {
+      const newValStr = String(newVal)
+      const oldValStr = oldVal ? String(oldVal) : ''
+      isLenChange.value = oldVal ? newValStr.length !== oldValStr.length : false
+      isIncrease.value = oldVal ? newVal > oldVal : false
+      isNegative.value = newVal < 0
+      const newValList = newValStr.split('')
+      numberList.value = newValList
+    }, {
+      immediate: true,
+    })
+
     return () => {
-      return h(
-        Transition,
-        {
-          name: 'animations-number',
-          mode: 'out-in',
-        },
-        {
-          default: () => h('span', {
-            class: 'animations-number-container',
-            key: props.number,
-          }, h('span', props.number)),
-        },
-      )
+      const numberListVal = numberList.value
+      return h(Transition, {
+        name: isIncrease.value ? 'animations-number-container-down' : 'animations-number-container-up',
+        mode: 'out-in',
+      }, {
+        default: () => h('span', {
+          key: isLenChange.value ? Date.now() : 'len',
+          class: 'animations-number-container',
+        }, numberListVal.map((n, idx) =>
+          h(Transition, {
+            name: isIncrease.value ? 'animations-number-item-down' : 'animations-number-item-up',
+            mode: 'out-in',
+          }, {
+            default: () => h('span', {
+              class: 'animations-number-item',
+              key: idx - 1 < 0 ? n : numberListVal[idx - 1] + n,
+            }, n),
+          }),
+        )),
+      })
     }
   },
   {
